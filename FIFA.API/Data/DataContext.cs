563fbae48a8e2cc26bee718171b452d098e04bde
CamiloUB <126737141+CamiloUB@users.noreply.github.com>
2023-08-16 13:44:32 -0500

    First Migration

---
+++ FIFA.API/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using FIFA.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+// En DataContext se genera la conexión a la BD
+// Y lo que se hace acá sirve para mapear las entidades a la BD
+
+namespace FIFA.API.Data
+{
+    public class DataContext:DbContext // los dos puntos refieren a una herencia que es de donde nos vamos a conectar a la BD
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options) // Este es el constructor, y el DbContext está apuntando a options
+        {
+                
+        }
+
+        public DbSet<Country>Countries { get; set; } // es el que me detecta que es una tabla y que la va mandar a la BD. A cada entidad hay que hacerle
+  
+        // Todo lo que se siga a la entidad hace las veces de controlador(en este caso el plural de la entidad)
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique(); // Con esto garantizamos que el nombre(de esta entidad) no se repita
+        }
+
+    }
+}
--- FIFA.API/Program.cs
+++ FIFA.API/Program.cs
@@ -1,3 +1,6 @@
+using FIFA.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+    builder.Services.AddDbContext<FIFA.API.Data.DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
+// Se hace esta inyección de servicio del SQL Server
 
 var app = builder.Build();
 
